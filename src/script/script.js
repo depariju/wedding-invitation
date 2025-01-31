a645ceb0bdad89ed9ac4f93600a105f7ec2d91b3
depariju <ridethesweetdreams@gmail.com>
2025-01-31 23:30:56 +0700

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,5 @@
 function GetUrlParameter(){
-    return window.location.search.substring(1).replaceAll("%20"," ").replaceAll("=","").toUpperCase();
+    return window.location.search.substring(1).replaceAll("%20"," ").replaceAll("=","").replaceAll("-"," ").toUpperCase();
 }
 if(GetUrlParameter()){
   $("#guest").html(GetUrlParameter());
